7516d1804455531265e1e65c6b60f762cb3ce218
Rhuan Carvalho <rsjlcarvalho@gmail.com>
2018-05-29 21:04:36 -0300

    [UPD] Collections

--- RocketStore.Domain/StoreContext/Entities/Customer.cs
+++ RocketStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,25 +1,36 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using RocketStore.Domain.StoreContext.ValueObjects;
 
 namespace RocketStore.Domain.StoreContext.Entities
 {
     public class Customer
     {
+        private readonly IList<Address> _addresses;
+
         public Customer(Name name, Document document, Email email, string phone)
         {
             Name = name;
             Document = document;
             Email = email;
             Phone = phone;
-            Addresses = new List<Address>();
+            _addresses = new List<Address>();
         }
 
         public Name Name { get; set; }
         public Document Document { get; private set; }
         public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public IReadOnlyCollection<Address> Addresses { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            // Validate address
+
+            // Add to addresses
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
